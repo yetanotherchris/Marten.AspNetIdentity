8b0684797d44675af59eceb3d109d63a9542adcf
Peter Davis <peter.w.davis@gmail.com>
2026-01-08 04:49:41 +1030

    Fix parallel integration test execution for all frameworks (#8)

--- src/Marten.AspNetIdentity.Tests/Integration/DocumentStoreManager.cs
+++ src/Marten.AspNetIdentity.Tests/Integration/DocumentStoreManager.cs
@@ -6,7 +6,18 @@ namespace Marten.AspNetIdentity.Tests.Integration
 	public class DocumentStoreManager
 	{
 		private static readonly ConcurrentDictionary<string, IDocumentStore> _documentStores = new ConcurrentDictionary<string, IDocumentStore>();
-		public static string ConnectionString => "host=localhost;port=5432;database=aspnetidentity;username=aspnetidentity;password=aspnetidentity;";
+		public static string ConnectionString => $"host=localhost;port=5432;database={DbName};username=aspnetidentity;password=aspnetidentity;";
+
+		private static string DbName =>
+		#if NET10_0
+			"aspnetidentity_net10";
+		#elif NET9_0
+			"aspnetidentity_net9";
+		#elif NET8_0
+			"aspnetidentity_net8";
+		#else
+			"aspnetidentity";
+		#endif
 
 		public static IDocumentStore GetMartenDocumentStore(Type testClassType, string connectionString = null)
 		{
