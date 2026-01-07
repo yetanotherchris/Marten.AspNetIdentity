2145d652e3f091b4449563a00d5abfca237ae22e
Peter Davis <peter.w.davis@gmail.com>
2026-01-07 21:23:10 +1030

    Fix parallel integration test execution for all frameworks

--- docker/startup.sql
+++ docker/startup.sql
@@ -1,3 +1,15 @@
 CREATE database aspnetidentity;
 CREATE USER aspnetidentity WITH PASSWORD 'aspnetidentity';
 ALTER USER aspnetidentity WITH SUPERUSER;
+
+CREATE DATABASE aspnetidentity_net8;
+CREATE USER aspnetidentity_net8 WITH PASSWORD 'aspnetidentity';
+ALTER USER aspnetidentity_net8 WITH SUPERUSER;
+
+CREATE DATABASE aspnetidentity_net9;
+CREATE USER aspnetidentity_net9 WITH PASSWORD 'aspnetidentity';
+ALTER USER aspnetidentity_net9 WITH SUPERUSER;
+
+CREATE DATABASE aspnetidentity_net10;
+CREATE USER aspnetidentity_net10 WITH PASSWORD 'aspnetidentity';
+ALTER USER aspnetidentity_net10 WITH SUPERUSER;
